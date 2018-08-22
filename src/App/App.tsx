320373f03d3aaf5a3b51e887c8d49f108a80ab8a
Artem Koziar <a@temich.in.ua>
2018-08-22 16:16:25 +0300

    Refactor App component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,19 +3,23 @@ import * as React from 'react';
 // tslint:disable-next-line
 import './App.css';
 
-import logo from './logo.svg';
+import image from './usain-bolt.jpg';
 
 class App extends React.Component {
   public render(): JSX.Element {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Reactive Pace Calculator</h1>
         </header>
-        <p className="App-intro">
+
+        <p className="App-content">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+
+        <footer className="App-footer">
+          <img src={image} className="App-image" alt="Usain Bolt" />
+        </footer>
       </div>
     );
   }
